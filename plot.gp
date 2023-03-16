5df8fadc79e46425832afa4e22aaccfa54a69aa9
Pablo C. Alcalde <paalcald@ucm.es>
2023-03-16 15:27:26 +0100

    Usando solve_ivp

--- plot.gp
+++ plot.gp
@@ -1,7 +1,8 @@
 outfile = 'apartado1.svg'
 set terminal svg
+set key off
 set output outfile
 set xrange [-1:1]
 set yrange [-1:1]
 plot 'vector_field.dat' using 1:2:3:4 with vectors, \
-     for [i=1:10] 'orbits.dat' using (column(2*i)):(column(2*i+1)) with lines
+     for [i=1:38] 'orbits.dat' using (column(2*i)):(column(2*i+1)) with lines
--- practica-3.py
+++ practica-3.py
@@ -4,7 +4,7 @@ import math
 from functools import partial
 from itertools import accumulate, repeat
 from typing import Callable
-#from scipy.integrate import solve_ivp
+from scipy.integrate import solve_ivp
 
 def get_vector_field(func: Callable,
                      t0: float,
@@ -43,20 +43,22 @@ INITIAL_PARAMETER_MIN_Y = -1
 INITIAL_PARAMETER_MAX_Y = 1
 VECTOR_GRID_GRANULARITY = 51
 
-INITIAL_VALUES = [[p0, 0.1]
-                  for p0 in np.linspace(0.1,
-                                        1,
-                                        9)]
+INITIAL_VALUES = ([[p0, 0.1]
+                   for p0 in np.linspace(-1,
+                                         1,
+                                         19)]
+                  +
+                  [[0.1, q0]
+                   for q0 in np.linspace(-1,
+                                         1,
+                                         19)])
+
 A_ORBIT_PARAMETER = 3
 B_ORBIT_PARAMETER = .5
 
-q0 = 0.5
-p0 = 0.5
 t0 = 0
 tN = 10
 delta = 1e-3
-a = 3
-b = .5
 t = np.arange(t0, tN, delta)
 
 
@@ -64,20 +66,9 @@ def general_orbit(t, z, a, b):
     q, p = z
     return [2* p, - (4 * q / a) * (q * q - b)]
 
-def general_orbit_discretization(t, z, dt, a, b):
-    q_0, p_0 = z
-    q = 2 * p_0 * dt + q_0
-    p = p_0 - ( 4 * q_0 * dt / a) * ( q_0 * q_0 - b)
-    return q, p
-
 orbit = partial(general_orbit,
                 a = A_ORBIT_PARAMETER,
                 b = B_ORBIT_PARAMETER)
-
-orbit_discretization = partial(general_orbit_discretization,
-                               dt = delta,
-                               a = A_ORBIT_PARAMETER,
-                               b = B_ORBIT_PARAMETER)
 
 vector_field = get_vector_field(orbit,
                                 0,
@@ -86,24 +77,11 @@ vector_field = get_vector_field(orbit,
                                 -1,#INITIAL_PARAMETER_MIN_Y,
                                 1,#INITIAL_PARAMETER_MAX_Y,
                                 VECTOR_GRID_GRANULARITY)
-def solve_ivp(discretization, initial_value, t):
-    solution = np.zeros((t.shape[0],2))
-    current_value = initial_value
-    for i, tn in enumerate(t):
-        current_value = discretization(tn, current_value)
-        solution[i][0] = current_value[0]
-        solution[i][1] = current_value[1]
-    return solution
-    
-"""
+
 sol = np.column_stack([solve_ivp(orbit, [t0, tN], p0q0,
-                                 method='RK23',
                                  t_eval=t,
                                  ).y.T
                        for p0q0 in INITIAL_VALUES])
-"""
-sol = np.column_stack((solve_ivp(orbit_discretization, initial_value, t)
-                       for initial_value in INITIAL_VALUES))
 
 orbits_data = np.column_stack((t, sol))
 
